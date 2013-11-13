4757ab577b6f71479278f4c812406ff28b5b03c0
Fabian Knorr <ers.trion@gmail.com>
2013-11-13 16:20:34 +0100

    Sitzung vom 13.11.2013

--- Entwurf/src/de/joglearth/rendering/PlaneTessellator.java
+++ Entwurf/src/de/joglearth/rendering/PlaneTessellator.java
@@ -1,12 +1,13 @@
 package de.joglearth.rendering;
 
 import de.joglearth.geometry.Tile;
+import de.joglearth.surface.HeightMapManager;
 
 
 public class PlaneTessellator implements Tessellator {
 
 	@Override
-	public Mesh tessellateTile(Tile tile, int subdivisions) {
+	public Mesh tessellateTile(Tile tile, int subdivisions, HeightMapManager heightMap) {
 		// TODO Automatisch erstellter Methoden-Stub
 		return null;
 	}
--- Entwurf/src/de/joglearth/rendering/Tessellator.java
+++ Entwurf/src/de/joglearth/rendering/Tessellator.java
@@ -3,9 +3,10 @@ package de.joglearth.rendering;
 import de.joglearth.geometry.Tile;
 import de.joglearth.rendering.*;
 import de.joglearth.source.*;
+import de.joglearth.surface.HeightMapManager;
 import de.joglearth.ui.*;
 
 
 public interface Tessellator {
-	Mesh tessellateTile(Tile tile, int subdivisions);
+	Mesh tessellateTile(Tile tile, int subdivisions, HeightMapManager heightMap);
 }
