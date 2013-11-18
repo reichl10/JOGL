fecf8c92ba48d676e044bc5181ec1d3861fc823c
Constantin Wenger <constantin.wenger@googlemail.com>
2013-11-18 20:14:35 +0100

    Added some java docs to Settings.java

--- Entwurf/src/de/joglearth/settings/Settings.java
+++ Entwurf/src/de/joglearth/settings/Settings.java
@@ -3,78 +3,217 @@ package de.joglearth.settings;
 import javax.tools.JavaFileManager.Location;
 
 
-public class Settings {	
-	private static Settings instance = null;
-	
+
+/**
+ * Settings
+ */
+public class Settings {
+
+    /**
+     * Class to store settings.
+     */
+    private static Settings instance = null;
+
+
+    /**
+     * Get the instance of Settings.
+     * 
+     * @return the settings
+     */
     public static Settings getInstance() {
-		if (instance == null)
-			instance = new Settings();
-		return instance;
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
     }
 
+    /**
+     * Add a SettingsListener to be called if setting with the given name is
+     * changed.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param listener
+     *            the listener to be called
+     */
     public void addSettingsListener(String property, SettingsListener listener) {
 
     }
 
+    /**
+     * Remove a SettingsListener from a setting with the given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param listener
+     *            the listener to remove
+     */
     public void removeSettingsListener(String property, SettingsListener listener) {
 
     }
 
-    public void putInteger(String property, int value) {
-    	
-    }
-
-    public void putDouble(String property, double value) {
-    	
-    }
-
-    public void putFloat(String property, float value) {
-
-    }
-
-    public void putLong(String property, long value) {
+    /**
+     * Stores a setting of type Integer using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putInteger(final String property, Integer value) {
 
     }
 
-    public void putLocation(String property, Location value) {
+    /**
+     * Stores a setting of type Double using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putDouble(final String property, Integer value) {
 
     }
 
-    public void putBoolean(String property, boolean value) {
-    	
-    }
-    
-    public void putString(String property, String value) {
+    /**
+     * Stores a setting of type Float using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putFloat(final String property, Float value) {
 
     }
-    
-    public boolean getBoolean(String property) {
-    	return false;
+
+    /**
+     * Stores a setting of type Long using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putLong(String property, Long value) {
+
     }
 
-    public String getString(String property) {
-    	return null;
+    /**
+     * TODO: Ask if property can be lost here.
+     */
+    public void putLocation(final String property, Location value) {
+
     }
 
-    public long getLong() {
-    	return 0L;
+    /**
+     * Stores a setting of type Boolean using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putBoolean(final String property, Boolean value) {
+
     }
 
-    public float getFloat(String property) {
-    	return 0;
+    /**
+     * Stores a setting of type String using a given name.
+     * 
+     * @param property
+     *            the name of the setting
+     * @param value
+     *            the value of the setting
+     */
+    public void putString(final String property, String value) {
+
     }
 
-    public double getDouble(String property) {
-    	return 0;
+    /**
+     * Retrieve the setting stored, using the given key, as Boolean.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as Boolean or null if no
+     *         setting found with given name or the setting is no instance of
+     *         Boolean
+     */
+    public final Boolean getBoolean(final String property) {
+        return null;
     }
 
-
-    public int getInteger(String property) {
-    	return 0;
+    /**
+     * Retrieve the setting stored, using the given key, as String.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as String or null if no
+     *         setting found with given name or the setting is no instance of
+     *         String
+     */
+    public final String getString(String property) {
+        return null;
     }
 
+    /**
+     * Retrieve the setting stored, using the given key, as Long.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as Long or null if no
+     *         setting found with given name or the setting is no instance of
+     *         Long
+     */
+    public Long getLong(String property) {
+        return null;
+    }
+
+    /**
+     * Retrieve the setting stored, using the given key, as Float.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as Float or null if no
+     *         setting found with given name or the setting is no instance of
+     *         Float
+     */
+    public Float getFloat(String property) {
+        return null;
+    }
+
+    /**
+     * Retrieve the setting stored, using the given key, as Double.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as Double or null if no
+     *         setting found with given name or the setting is no instance of
+     *         Double
+     */
+    public Double getDouble(String property) {
+        return null;
+    }
+
+    /**
+     * Retrieve the setting stored, using the given key, as Integer.
+     * 
+     * @param property
+     *            the name of the setting
+     * @return The setting stored under the given key as Integer or null if no
+     *         setting found with given name or the setting is no instance of
+     *         Integer
+     */
+    public Integer getInteger(String property) {
+        return null;
+    }
+
+    /**
+     * TODO: Ask what he thought there, why there is a property;
+     */
     public Location[] getLocations(String property) {
-    	return null;
+        return null;
     }
-    
+
 }
