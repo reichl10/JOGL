af1c06d96a4eafe5b3c2c0d950afd2cc1ebe65a1
Constantin Wenger <constantin.wenger@googlemail.com>
2013-11-19 13:10:49 +0100

    Java Doc to Class SettingsContract

--- Entwurf/src/de/joglearth/settings/SettingsContract.java
+++ Entwurf/src/de/joglearth/settings/SettingsContract.java
@@ -1,23 +1,54 @@
 package de.joglearth.settings;
 
+/**
+ * Class that contains Constants and static methods to work with the
+ * {@link de.joglearth.settings.Settings Settings} class.
+ */
 public final class SettingsContract {
-    public static final String LANGUAGE = "lang";
-    public static final String TEXTURE_FILTER = "textureFilter";
+
+    /**
+     * Name Constant for the Language setting.
+     */
+    public static final String LANGUAGE         = "lang";
+    /**
+     * Name Constant for the texture filter setting.
+     */
+    public static final String TEXTURE_FILTER   = "textureFilter";
+    /**
+     * Name Constant for the Level of Details setting.
+     */
     public static final String LEVEL_OF_DETAILS = "lvlOfDetails";
+
+
     // TODO: Define others
-    
+
+    /**
+     * Private Constructor to prevent creating instances of this class.
+     */
     private SettingsContract() {}
-    
+
+    /**
+     * Inserts the default values for each of the settings defined in this
+     * contract.
+     */
     public static void setDefaultSettings() {
-    	
-    }
-    
-    public static void loadSettings() {
-    	
+
     }
 
-    public static void saveSettings() {
-    	
+    /**
+     * Loads the values for the settings defined in this contract from a file.
+     * This loads from the same files the {@link #saveSettings()} saves to.
+     */
+    public static void loadSettings() {
+
     }
-    
+
+    /**
+     * Saves the settings defined in this contract to a file.
+     * This saves to the same files the {@link #loadSettings()} loads them from.
+     */
+    public static void saveSettings() {
+
+    }
+
 }
