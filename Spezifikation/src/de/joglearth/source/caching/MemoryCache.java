46502c3e238a547a1db4ed5511fc0d6890d8671a
Sebastian Reichl <Sebastian.Reichl@gmx.de>
2013-11-26 22:30:10 +0100

    Javadocs bei source.caching geändert

--- Spezifikation/src/de/joglearth/source/caching/MemoryCache.java
+++ Spezifikation/src/de/joglearth/source/caching/MemoryCache.java
@@ -6,41 +6,41 @@ import de.joglearth.source.SourceListener;
 import de.joglearth.source.SourceResponse;
 import de.joglearth.source.SourceResponseType;
 
+/**
+ * Implements the {@link Cache} interface in such way that it stores objects in the memory.
+ * 
+ */
+public class MemoryCache<Key, Value> implements Cache<Key, Value> {
 
-// Cache-Reference-Typ ist Integer, da die interne Speicherung wohl
-// mit Arrays oder HashMap<Integer, Value> funktioniert
-public class MemoryCache<Key, Value> 
-		implements Cache<Key, Value> {
+    private HashMap<Key, Value> storage = new HashMap<Key, Value>();
 
-	private HashMap<Key, Value> storage = new HashMap<Key, Value>();
 
-	@Override
-	public SourceResponse<Value> requestObject(Key key,
-			SourceListener<Key, Value> sender) {
-		Value object = storage.get(key);
-		SourceResponseType responseType = object != null 
-				? SourceResponseType.SYNCHRONOUS :  SourceResponseType.MISSING;
-		return new SourceResponse<Value>(responseType, object);
-	}
+    @Override
+    public SourceResponse<Value> requestObject(Key key,
+            SourceListener<Key, Value> sender) {
+        Value object = storage.get(key);
+        SourceResponseType responseType = object != null
+                ? SourceResponseType.SYNCHRONOUS : SourceResponseType.MISSING;
+        return new SourceResponse<Value>(responseType, object);
+    }
 
-	@Override
-	public void putObject(Key key, Value value) {
-		storage.put(key, value);
-	}
+    @Override
+    public void putObject(Key key, Value value) {
+        storage.put(key, value);
+    }
 
-	@Override
-	public void dropObject(Key key) {
-		storage.remove(key);
-	}
+    @Override
+    public void dropObject(Key key) {
+        storage.remove(key);
+    }
 
-	@Override
-	public Iterable<Key> getExistingObjects() {
-		return storage.keySet();
-	}
+    @Override
+    public Iterable<Key> getExistingObjects() {
+        return storage.keySet();
+    }
 
-	@Override
-	public void dropAll() {
-		storage.clear();
-	}
-
+    @Override
+    public void dropAll() {
+        storage.clear();
+    }
 }
--- Spezifikation/src/de/joglearth/source/caching/PathTranslator.java
+++ Spezifikation/src/de/joglearth/source/caching/PathTranslator.java
@@ -1,7 +1,21 @@
 package de.joglearth.source.caching;
 
-
+/**
+ * Is used to convert a path to a <code>Key</code> and vice versa.
+ */
 public interface PathTranslator<Key> {
+    
+    /**
+     * Converts a <code>Key</code> into a path.
+     * @param k The <code>Key</code> that be resolved to a string
+     * @return The string representation of the <code>Key</code>
+     */
     String toFileSystemPath(Key k);
+    
+    /**
+     * Converts a path to a <code>Key</code>.
+     * @param s The path that should be resolved to a <code>Key</code>
+     * @return The <code>Key</code> described by the path
+     */
     Key fromFileSystemPath(String s);
 }
