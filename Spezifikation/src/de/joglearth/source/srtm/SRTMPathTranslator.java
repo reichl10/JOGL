ae7acf7f16ec9f73f3cbf8551d446d40ce8dbc82
Sebastian Reichl <Sebastian.Reichl@gmx.de>
2013-11-27 14:26:41 +0100

    Source fertig

--- Spezifikation/src/de/joglearth/source/srtm/SRTMPathTranslator.java
+++ Spezifikation/src/de/joglearth/source/srtm/SRTMPathTranslator.java
@@ -10,13 +10,11 @@ public class SRTMPathTranslator implements PathTranslator<SRTMTileIndex> {
 
     @Override
     public String toFileSystemPath(SRTMTileIndex k) {
-        // TODO Automatisch generierter Methodenstub
         return null;
     }
 
     @Override
     public SRTMTileIndex fromFileSystemPath(String s) {
-        // TODO Automatisch generierter Methodenstub
         return null;
     }
 
--- Spezifikation/src/de/joglearth/source/srtm/SRTMTile.java
+++ Spezifikation/src/de/joglearth/source/srtm/SRTMTile.java
@@ -1,25 +1,27 @@
 package de.joglearth.source.srtm;
 
 /**
- * Provides access to two dimensional SRTM height data.
+ * Provides access to two-dimensional SRTM height data.
  */
 public final class SRTMTile {
-    
+
     /**
-     * Constructor. Decodes the height information from binary SRTM data. 
+     * Constructor. Decodes the height information from binary SRTM data.
+     * 
      * @param raw The binary data
      */
     public SRTMTile(byte[] raw) {
-        
+
     }
-    
+
     /**
-     * Returns a short array containing .... ina given level of detail.
+     * Returns a two-dimensional array containing the height data interpolated to 1/(4^lod) the
+     * size.
      * 
      * @param lod The level of detail of the data
-     * @return 
+     * @return Two-dimensional array containing the height data
      */
-    public short[][] getTile(int lod)  {
+    public short[][] getTile(int lod) {
         return null;
     }
 }
