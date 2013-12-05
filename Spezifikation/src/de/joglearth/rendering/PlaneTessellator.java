55565fbea5657707f1f34ef561beb85f79e8d7d1
Fabian Knorr <ers.trion@gmail.com>
2013-12-05 13:59:00 +0100

    PlaneTessellator angefangen

--- Spezifikation/src/de/joglearth/rendering/PlaneTessellator.java
+++ Spezifikation/src/de/joglearth/rendering/PlaneTessellator.java
@@ -1,6 +1,8 @@
 package de.joglearth.rendering;
 
 import de.joglearth.geometry.Tile;
+import static java.lang.Math.*;
+import static de.joglearth.rendering.MeshUtils.*;
 
 
 /**
@@ -10,7 +12,45 @@ public class PlaneTessellator implements Tessellator {
 
     @Override
     public Mesh tessellateTile(Tile tile, int subdivisions, boolean heightMap) {
-        return null;
+        int nVertices = subdivisions + 2;
+        double latStep = PI / pow(2, tile.getDetailLevel()), lonStep = 2 * latStep;
+        float[] vertices = new float[8 * nVertices * nVertices];
+        int vertIndex = 0;
+        double lon = tile.longitudeFrom(), lat = tile.latitudeFrom();
+
+        for (int line = 0; line < nVertices; ++line) {
+            for (int col = 0; col < nVertices; ++col) {
+                writeVertex(vertices, vertIndex, (float) lon, (float) lat, 0);
+                writeTextureCoordinates(vertices, vertIndex, (float) col / nVertices, (float) line
+                        / nVertices);
+                lon += lonStep;
+                lat += latStep;
+            }
+        }
+
+        vertIndex = 0;
+        for (int line = 0; line < nVertices; ++line) {
+            for (int col = 0; col < nVertices; ++col) {
+                writeNormal(vertices, vertIndex, 0, 0, 1);
+                lon += lonStep;
+                lat += latStep;
+            }
+        }
+
+        int[] indices = new int[6 * (nVertices - 1) * (nVertices - 1)];
+        int indIndex = 0;
+
+        for (int line = 0; line < nVertices - 1; ++line) {
+            for (int col = 0; col < nVertices - 1; ++col) 
+            {
+                indices[indIndex+0] = 0;
+                indices[indIndex+1] = 0;
+                indices[indIndex+2] = 0;
+                indIndex += 3;
+            }
+        }
+
+        return new Mesh(vertices, VERTEX_FORMAT, indices);
     }
 
 }
