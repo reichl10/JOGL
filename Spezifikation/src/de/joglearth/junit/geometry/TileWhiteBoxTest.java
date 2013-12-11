f1bf5525536eb3e27c45b927f54300291be9de8d
Sebastian Reichl <Sebastian.Reichl@gmx.de>
2013-12-11 14:21:19 +0100

    WhiteBoxTest fuer tile

---
+++ Spezifikation/src/de/joglearth/junit/geometry/TileWhiteBoxTest.java
@@ -0,0 +1,20 @@
+package de.joglearth.junit.geometry;
+
+import static java.lang.Math.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.joglearth.geometry.Tile;
+
+
+public class TileWhiteBoxTest {
+
+    @Test
+    public void test() {
+        System.out.println(new Tile(4, 8, 6));
+        Tile t = new Tile(4, 8, 6);
+        assertTrue(t.intersects(PI, 0, -PI/2, PI/4));
+    }
+
+}
