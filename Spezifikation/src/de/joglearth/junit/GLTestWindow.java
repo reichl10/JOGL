cc21eed098e00bf51db5aa6a41589ab3566bbaed
Fabian Knorr <ers.trion@gmail.com>
2013-12-11 17:32:49 +0100

    Am test rumgedoktert

--- Spezifikation/src/de/joglearth/junit/GLTestWindow.java
+++ Spezifikation/src/de/joglearth/junit/GLTestWindow.java
@@ -1,8 +1,5 @@
 package de.joglearth.junit;
 
-import java.awt.EventQueue;
-import java.lang.reflect.InvocationTargetException;
-
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
@@ -10,6 +7,8 @@ import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.awt.GLCanvas;
 import javax.swing.JFrame;
+
+import com.jogamp.opengl.util.Animator;
 
 import de.joglearth.rendering.GLError;
 import de.joglearth.util.AWTInvoker;
@@ -22,8 +21,15 @@ public class GLTestWindow {
 
     private JFrame frame;
     private GLCanvas canvas;
-    private boolean ready;
+    private Animator anim;
+    private boolean quit;
+    private boolean insideFrame;
+    private Object frameMonitor = new Object();
 
+
+    private synchronized boolean isInsideFrame() {
+        return insideFrame;
+    }
 
     /**
      * Constructor.
@@ -33,6 +39,8 @@ public class GLTestWindow {
      */
     public GLTestWindow() {
         ClearableEventQueue.impose();
+        insideFrame = false;
+        final GLTestWindow that = this;
         AWTInvoker.invoke(new Runnable() {
 
             @Override
@@ -40,8 +48,8 @@ public class GLTestWindow {
                 frame = new JFrame("GL Test Window");
                 frame.setSize(400, 300);
 
-                ready = false;
-                canvas = new GLCanvas(new GLCapabilities(GLProfile.getGL2ES1()));
+                quit = false;
+                canvas = new GLCanvas(new GLCapabilities(GLProfile.getDefault()));
 
                 canvas.addGLEventListener(new GLEventListener() {
 
@@ -49,26 +57,33 @@ public class GLTestWindow {
                     public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {}
 
                     @Override
-                    public void init(GLAutoDrawable arg0) {}
+                    public void init(GLAutoDrawable arg0) {
+                        anim = new Animator(arg0);
+                    }
 
                     @Override
                     public void dispose(GLAutoDrawable arg0) {}
 
                     @Override
                     public void display(GLAutoDrawable arg0) {
-                        GL2 gl = canvas.getGL().getGL2();
-                        gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-                        GLError.throwIfActive(gl);
-                        ready = true;
+                        synchronized (frameMonitor) {
+                            frameMonitor.notify();
+                        }
+                        synchronized (that) {
+                            try {
+                                that.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            if (!quit) {
+                                arg0.display();
+                            }
+                        }
                     }
                 });
 
                 frame.add(canvas);
                 frame.setVisible(true);
-
-                while (!ready) {
-                    ClearableEventQueue.getInstance().clear();
-                }
             }
 
         });
@@ -92,10 +107,27 @@ public class GLTestWindow {
         return canvas.getGL().getGL2();
     }
 
+    public void beginFrame() {
+        synchronized (frameMonitor) {
+            try {
+                frameMonitor.wait();
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public synchronized void endFrame() {
+        notify();
+    }
+
     /**
      * Disposes the frame.
      */
     public void dispose() {
+        beginFrame();
+        synchronized (this) {
+            quit = true;
+            notify();
+        }
         frame.dispose();
     }
 
