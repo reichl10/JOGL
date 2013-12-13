066a9defae75e05d7f0ca7e6175910c089726b91
Christof <blauberg@fim.uni-passau.de>
2013-12-13 10:54:18 +0100

    HTTP Request um Encoding erweitert

--- Spezifikation/src/de/joglearth/util/HTTP.java
+++ Spezifikation/src/de/joglearth/util/HTTP.java
@@ -4,10 +4,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 
 /**
@@ -24,19 +29,49 @@ public final class HTTP {
     private HTTP() {
 
     }
-    
+
     /**
      * Gathers information via a HTTP get-request and is used for synchronous HTTP queries.
      * 
      * @param url Address of a server
+     * @param getRequest An <code>Iterable</code> of get parameters in the form {key1, value1, key2,
+     *        value2, ...}
      * @return Content of the HTTP response. Null if no content is available or a HTTP Error
      *         occurred.
      * @throws IllegalArgumentException If the url is not well formed, e.g. <code>null</code>
      */
-    public static byte[] get(String url) throws IllegalArgumentException {
+    public static byte[] get(String url, Iterable<String> getRequest)
+            throws IllegalArgumentException {
         if (url == null) {
             throw new IllegalArgumentException("URL was null!");
         }
+
+        if (url.contains("?")) {
+            throw new IllegalArgumentException("Not wellformed URL");
+        }
+
+        StringBuilder request = new StringBuilder();
+        if (getRequest != null) {
+            int i = 0;
+            for (String s : getRequest) {
+                try {
+                    if (i == 0) {
+                        request.append('?');
+                    } else if (i % 2 == 0) {
+                        request.append('&');
+                    }
+                    request.append(URLEncoder.encode(s, "UTF-8"));
+                    if (i % 2 == 0) {
+                        request.append('=');
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    throw new IllegalArgumentException("Request not well formed");
+                }
+                i++;
+            }
+        }
+
+        url = url + request.toString();
 
         URL serverUrl;
         try {
@@ -86,19 +121,65 @@ public final class HTTP {
      * Creates a HTTP post-request and is used for synchronous HTTP queries.
      * 
      * @param url Address of a server
-     * @param request Post request
+     * @param request Post request. Not allowed to be <code>null</code>;
      * @return Content of the HTTP response. Null if no content is available or a HTTP Error
      *         occurred.
      * @throws IllegalArgumentException If the url or request is not well formed, e.g.
      *         <code>null</code>
      */
-    public static byte[] post(String url, String request) throws IllegalArgumentException {
+    public static byte[] post(String url, Iterable<String> getRequest, Iterable<String> postRequest)
+            throws IllegalArgumentException {
         if (url == null) {
             throw new IllegalArgumentException("URL was null!");
         }
-        if (request == null) {
-            throw new IllegalArgumentException("Request was null!");
+
+        if (url.contains("?")) {
+            throw new IllegalArgumentException("Not wellformed URL");
         }
+
+        if (postRequest == null) {
+            throw new IllegalArgumentException("Post request was null");
+        }
+
+        StringBuilder request = new StringBuilder();
+        if (getRequest != null) {
+            int i = 0;
+            for (String s : getRequest) {
+                try {
+                    if (i == 0) {
+                        request.append('?');
+                    } else if (i % 2 == 0) {
+                        request.append('&');
+                    }
+                    request.append(URLEncoder.encode(s, "UTF-8"));
+                    if (i % 2 == 0) {
+                        request.append('=');
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    throw new IllegalArgumentException("Request not well formed");
+                }
+                i++;
+            }
+        }
+
+        StringBuilder post = new StringBuilder();
+        int i = 0;
+        for (String s : postRequest) {
+            try {
+                if (i % 2 == 0 && i > 0) {
+                    post.append('&');
+                }
+                post.append(URLEncoder.encode(s, "UTF-8"));
+                if (i % 2 == 0) {
+                    post.append('=');
+                }
+            } catch (UnsupportedEncodingException e) {
+                throw new IllegalArgumentException("Request not well formed");
+            }
+            i++;
+        }
+
+        url = url + request.toString();
 
         URL serverUrl;
         try {
@@ -126,7 +207,7 @@ public final class HTTP {
         DataOutputStream dos;
         try {
             dos = new DataOutputStream(connection.getOutputStream());
-            dos.writeBytes(request);
+            dos.writeBytes(post.toString());
             dos.flush();
             dos.close();
         } catch (IOException e) {
@@ -167,4 +248,14 @@ public final class HTTP {
 
         return response;
     }
+
+    public static void main(String args[]) {
+
+        ArrayList<String> bla = new ArrayList<String>();
+        bla.add("login");
+        bla.add("Port Albo Straße+-µ ?&32!");
+        String test = new String(get("http://127.0.0.1/post.php", bla));
+        System.out.println(test);
+    }
+
 }
