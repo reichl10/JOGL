f63243a790486259a32141d3a9a72a84e30e3c4f
Constantin Wenger <constantin.wenger@googlemail.com>
2013-12-13 03:13:20 +0100

    Void Vector3 Test

---
+++ Spezifikation/src/de/joglearth/junit/geometry/Vector3Test.java
@@ -0,0 +1,68 @@
+package de.joglearth.junit.geometry;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import de.joglearth.geometry.Vector3;
+
+
+public class Vector3Test {
+
+    @Test
+    public final void testVector3() {
+        fail("Not yet implemented"); // TODO
+        Vector3 vector3 = new Vector3(0.1, 0.2, 0.3);
+    }
+
+    @Test
+    public final void testVector3DoubleDoubleDouble() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testTo() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testTimes() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testPlus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testMinus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testLength() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testAdd() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testNormalized() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testCrossProduct() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testEqualsObject() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
