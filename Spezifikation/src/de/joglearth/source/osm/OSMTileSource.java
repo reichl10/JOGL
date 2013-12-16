b891252ed3567e11bc441ff748a7b89e2889a7e8
Gabi Haas <haasgab@fim.uni-passau.de>
2013-12-16 19:43:56 +0100

    SRTMBinarySource, OSMTileSource angefangen, MemoryCache formatiert

--- Spezifikation/src/de/joglearth/source/osm/OSMTileSource.java
+++ Spezifikation/src/de/joglearth/source/osm/OSMTileSource.java
@@ -1,7 +1,10 @@
 package de.joglearth.source.osm;
 
+import de.joglearth.geometry.GeoCoordinates;
+import de.joglearth.geometry.Tile;
 import de.joglearth.source.*;
 import de.joglearth.surface.TiledMapType;
+import de.joglearth.util.HTTP;
 
 
 /**
@@ -9,13 +12,8 @@ import de.joglearth.surface.TiledMapType;
  */
 public class OSMTileSource implements Source<OSMTile, byte[]> {
 
-    // Die Server, die der Reihe nach nach Daten gefragt werden sollen.
-    // K�nnen entwerder einfache URLs a la "domain.tld/folder/to/data/" sein,
-    // oder ein Format-String a la "domain.tld/data/%s/%s", der dann mit einer
-    // �berschriebenen getURL-Methode aufgel�st wird.
-
-    // Verwaltung? Sollen u.U. unerreichbare Server markiert werden?
     private final String[] servers;
+    private int offset = 0;
 
 
     /**
@@ -31,13 +29,53 @@ public class OSMTileSource implements Source<OSMTile, byte[]> {
     public SourceResponse<byte[]> requestObject(OSMTile k, SourceListener<OSMTile, byte[]> sender) {
         return null;
     }
-    
+
+    private byte[] getOSMTile(Tile tile) {
+
+        int y = (int) (((tile.getLatitudeFrom() + tile.getLatitudeTo()) / 2) / 180 * Math.PI);
+        int x = (int) ((tile.getLongitudeFrom() + tile.getLongitudeTo()) / 2);
+
+        int zoom = 0;
+        int n = (int) Math.pow(2, zoom); // TODO n = 2^zoom
+        int xtile = n * ((x + 180) / 360);
+        int ytile = (int) (n * (1 - (Math.log(Math.tan(y) + 1 / Math.cos(y)) / Math.PI)) / 2);
+
+        StringBuilder builder = new StringBuilder();
+        byte[] response = null;
+        
+        int i = 0;
+        while (response == null && i < servers.length) {
+            builder.append(servers[offset]);
+            builder.append("/");
+            builder.append(zoom);
+            builder.append("/");
+            builder.append(xtile);
+            builder.append("/");
+            builder.append(ytile);
+            builder.append(".png");
+            
+            response = HTTP.get(builder.toString(), null);
+            
+            i++;
+            
+            if (response == null) {
+                offset++;
+            }
+            
+            if (offset == servers.length) {
+                offset = 0;
+            } 
+        }
+
+        return null;
+    }
+
     /**
      * Sets the type of an OpenStreetMap tile.
      * 
      * @param type <code>TiledMapType</code> of the tile
      */
     public void setTileType(TiledMapType type) {
-
+        //TODO Zum Type passende Server besorgen.
     }
 }
--- Spezifikation/src/de/joglearth/source/srtm/SRTMBinarySource.java
+++ Spezifikation/src/de/joglearth/source/srtm/SRTMBinarySource.java
@@ -3,6 +3,7 @@ package de.joglearth.source.srtm;
 import de.joglearth.source.Source;
 import de.joglearth.source.SourceListener;
 import de.joglearth.source.SourceResponse;
+import de.joglearth.source.SourceResponseType;
 import de.joglearth.util.HTTP;
 
 
@@ -14,13 +15,9 @@ import de.joglearth.util.HTTP;
  */
 public class SRTMBinarySource implements Source<SRTMTileIndex, byte[]> {
 
-    // Die Server, die der Reihe nach nach Daten gefragt werden sollen.
-    // K�nnen entwerder einfache URLs a la "domain.tld/folder/to/data/" sein,
-    // oder ein Format-String a la "domain.tld/data/%s/%s", der dann mit einer
-    // �berschriebenen getURL-Methode aufgel�st wird.
-
-    // Verwaltung? Sollen u.U. unerreichbare Server markiert werden?
-    private final String[] servers = null;
+    private static final int SMALLEST_NUMBER_WITH_THREE_DIGITS = 100;
+    private static final int SMALLEST_NUMBER_WITH_TWO_DIGITS = 10;
+    private final String server = "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/";
 
 
     /**
@@ -32,7 +29,210 @@ public class SRTMBinarySource implements Source<SRTMTileIndex, byte[]> {
     @Override
     public SourceResponse<byte[]> requestObject(SRTMTileIndex key,
             SourceListener<SRTMTileIndex, byte[]> sender) {
-        // TODO Automatisch generierter Methodenstub
+
+        byte[] response = null;
+
+        // TODO ExecutorService?!
+        response = getSRTMTile(key);
+
+        if (sender != null) {
+            sender.requestCompleted(key, response);
+        }
+
+        return new SourceResponse<byte[]>(SourceResponseType.ASYNCHRONOUS, response);
+    }
+
+    private byte[] getSRTMTile(SRTMTileIndex tile) {
+
+        String[] url = getUrl(tile.longitude, tile.latitude);
+
+        for (int i = 0; i < url.length; ++i) {
+
+            if (url[i] == null) {
+                return null;
+            }
+
+            byte[] response = HTTP.get(url[i], null);
+
+            if (response != null) {
+                return response;
+            }
+
+        }
+
         return null;
+    }
+
+    /*
+     * Nordamerika: W170 - W040; N15 - N60.
+     * 
+     * Südamerika: W095 - W030; N10 - S60.
+     * 
+     * Europa und Asien: W015 - E180 && W180 - W135; N60 - S15.
+     * 
+     * Afrika: W035 - E65; N40 - S35.
+     * 
+     * Australien: E110 - E180 && W180 - W105; S10 - S45.
+     * 
+     * Alle mit einer Auflösung von 3 Bogensekunden (SRTM-3).
+     * 
+     * Wenn nichts gefunden wird, dann immer noch bei den Inseln gucken.
+     * 
+     * Äquator spezifiziert durch N00, Längengrad 0 spezifiziert durch E000.
+     */
+    private String[] getUrl(int longitude, int latitude) {
+        int offset = 0;
+        //TODO Parse Lon/Lat
+        int lon = longitude; // E or -W
+        int lat = latitude; // N or -S
+        String[] url = new String[6];
+
+        StringBuilder builder = null;
+
+        if (lon >= -170 && lon <= -40 && lat >= 15 && lat <= 60) {
+            // Nordamerika
+
+            builder = new StringBuilder(server);
+
+            builder.append("North_America/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        } else if (lon >= -95 && lon <= -30 && lat <= 10 && lat >= -60) {
+            // Südamerika
+
+            builder = new StringBuilder(server);
+
+            builder.append("South_America/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        } else if (((lon >= -15 && lon <= 180) || (lon >= -180 && lon <= -135)) && lat <= 60
+                && lat >= -15) {
+            // Europa und Asien
+
+            builder = new StringBuilder(server);
+
+            builder.append("Eurasia/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        } else if (lon >= -35 && lon <= 65 && lat <= 40 && lat >= -35) {
+            // Afrika
+
+            builder = new StringBuilder(server);
+
+            builder.append("Africa/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        } else if (((lon >= 110 && lon <= 180) || (lon >= -180 && lon <= -105)) && lat <= -10
+                && lat >= -45) {
+            // Australien
+
+            builder = new StringBuilder(server);
+
+            builder.append("Australia/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        } else {
+
+            builder = new StringBuilder(server);
+
+            builder.append("Islands/");
+
+            parseLat(builder, lat);
+            parseLon(builder, lon);
+
+            builder.append(".hgt.zip");
+
+            url[offset] = builder.toString();
+
+            offset++;
+
+        }
+
+        return url;
+    }
+
+    private void parseLon(StringBuilder builder, int lon) {
+        if (Math.abs(lon) < SMALLEST_NUMBER_WITH_TWO_DIGITS) {
+            if (lon < 0) {
+                builder.append("W00");
+            } else {
+                builder.append("E00");
+            }
+
+        } else if (lon < SMALLEST_NUMBER_WITH_THREE_DIGITS) {
+            if (lon < 0) {
+                builder.append("W0");
+            } else {
+                builder.append("E0");
+            }
+
+        } else {
+            if (lon < 0) {
+                builder.append("W");
+            } else {
+                builder.append("E");
+            }
+        }
+
+        builder.append(Math.abs(lon));
+    }
+
+    private void parseLat(StringBuilder builder, int lat) {
+        if (Math.abs(lat) < SMALLEST_NUMBER_WITH_TWO_DIGITS) {
+            if (lat < 0) {
+                builder.append("S0");
+            } else {
+                builder.append("N0");
+            }
+
+        } else {
+            if (lat < 0) {
+                builder.append("S");
+            } else {
+                builder.append("N");
+            }
+        }
+
+        builder.append(Math.abs(lat));
     }
 }
