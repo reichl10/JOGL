53e2ad772ddca00443ef433b316056bcd43507e1
Constantin Wenger <constantin.wenger@googlemail.com>
2013-12-19 23:57:07 +0100

    Debuggin Output to Reqest Distributor

--- Spezifikation/src/de/joglearth/source/caching/RequestDistributor.java
+++ Spezifikation/src/de/joglearth/source/caching/RequestDistributor.java
@@ -63,6 +63,7 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
     public void addCache(Cache<Key, Value> cache, int maxSize) {
         if (cache == null)
             return;
+        System.out.println("Added Cache "+cache.getClass().getName()+"  with Size: "+maxSize);
         if (maxSize < 1) {
             throw new IllegalArgumentException("Cache size should be > 0");
         }
@@ -315,6 +316,11 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
     private void addToCaches(Key k, Value v) {
         if (caches.size() < 1)
             return;
+        Cache topCache = caches.get(0);
+        Integer sizeOfCache = cacheSizeMap.get(topCache);
+        if (sizeOfCache < measure.getSize(v)) {
+            throw new RuntimeException("The Object is bigger then the Level1 Cache");
+        }
         addToCache(0, k, v);
     }
 
@@ -365,9 +371,12 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
     }
 
     private synchronized void makeSpaceInCache(int index, Integer space) {
-        if (caches.size() <= index)
+        if (caches.size() <= index || space == 0)
             return;
         Cache<Key, Value> cache = caches.get(index);
+        Integer cacheSize = cacheSizeMap.get(cache);
+        Integer spaceUsed = usedSizeMap.get(cache);
+        System.out.println("This Cache has "+cacheSize+" space, "+spaceUsed+" is used! We want: "+space);
         Map<Key, BigInteger> lastUsed = lastUsedMap.get(cache);
         Set<Entry<Key, BigInteger>> entrySet = lastUsed.entrySet();
         LinkedList<Entry<Key, BigInteger>> list = new LinkedList<Entry<Key, BigInteger>>(entrySet);
@@ -382,8 +391,10 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
         int spaceMade = 0;
         Set<CacheEntry> removedSet = new HashSet<CacheEntry>();
         boolean hasNextCache = caches.size() > index + 1;
+        System.out.println("Space we want: "+space);
         while (spaceMade < space && list.size() > 0) {
             System.out.println("Remove One from " + list.size());
+            
             Entry<Key, BigInteger> entry = list.pop();
             CacheMoveListener listener = new CacheMoveListener(Thread.currentThread());
             SourceResponse<Value> response = cache.requestObject(entry.getKey(), listener);
@@ -394,11 +405,12 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
                     removedSet.add(cEntry);
                 }
                 Integer sizeOfRemovedEntry = measure.getSize(response.value);
+                System.out.println("Removed Entry was worth: "+sizeOfRemovedEntry);
                 cache.dropObject(entry.getKey());
                 spaceMade += sizeOfRemovedEntry;
             } else if (response.response == SourceResponseType.ASYNCHRONOUS) {
-
                 try {
+                    System.out.println("Waiting for Async answer!");
                     wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -409,10 +421,12 @@ public class RequestDistributor<Key, Value> implements Source<Key, Value> {
                     removedSet.add(cEntry);
                 }
                 Integer sizeOfRemovedEntry = measure.getSize(listener.value);
+                System.out.println("Removed Entry was worth: "+sizeOfRemovedEntry);
                 cache.dropObject(listener.key);
                 spaceMade += sizeOfRemovedEntry;
 
             }
+            System.out.println("We allready made:"+spaceMade);
         }
         removeUsedSpace(cache, spaceMade);
         if (hasNextCache) {
