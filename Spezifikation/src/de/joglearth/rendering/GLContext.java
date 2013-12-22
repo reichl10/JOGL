bfaa46fe06e6cc2330ea349f1f42dfd588614738
Fabian Knorr <ers.trion@gmail.com>
2013-12-22 15:52:21 +0100

    GLContext-Dokus

--- Spezifikation/src/de/joglearth/rendering/GLContext.java
+++ Spezifikation/src/de/joglearth/rendering/GLContext.java
@@ -18,19 +18,36 @@ import de.joglearth.util.RunnableWithResult;
 import static javax.media.opengl.GL2.*;
 
 
+/**
+ * Encapsulates OpenGL calls and callbacks.
+ */
 public final class GLContext implements GLEventListener {
 
+    // The GL object
     private GL2 gl = null;
+
+    // The thread accessing the GL object
     private Thread glThread = null;
+
+    // The source drawable (e.g. a GLCanvas)
     private GLAutoDrawable drawable = null;
+
+    // All registered GLContextListeners
     private ArrayList<GLContextListener> listeners = new ArrayList<>();
-    private boolean insideDisplay = false, insideFrame = false;
+
+    // Whether the execution is currently inside a GLEventListener callback
+    private boolean insideCallback = false;
+
+    // Whether a frame is currently drawn
+    private boolean insideFrame = false;
 
 
+    // Structure holding tasks provided by invokeLater()
     private class Invocation {
 
         public Runnable runnable;
         public RunnableResultListener listener;
+
 
         public Invocation(Runnable r, RunnableResultListener l) {
             runnable = r;
@@ -40,16 +57,22 @@ public final class GLContext implements GLEventListener {
 
 
     private ArrayList<Invocation> pendingInvocations = new ArrayList<>();
-    private volatile boolean isPosted;
-    private volatile boolean isRunning;
+
+    // Whether postRedisplay() has been called and no new frame has begun since
+    private volatile boolean redisplayPending;
+
+    // Whether a rendering loop is currently active
+    private volatile boolean renderingLoopActive;
 
 
+    // Throws if init() has not been called yet
     private void assertIsInitialized() {
         if (drawable == null) {
             throw new IllegalStateException("GLContext has not yet been initialized");
         }
     }
 
+    // Throws if init() has not been called, or has been called with a different drawable
     private void assertIsInitialized(GLAutoDrawable caller) {
         assertIsInitialized();
         if (caller != drawable) {
@@ -58,14 +81,16 @@ public final class GLContext implements GLEventListener {
         }
     }
 
-    private void assertIsInsideDisplayFunction() {
-        if (!isInsideDisplayFunction()) {
+    // Throws if not inside a callback
+    private void assertIsInsideCallback() {
+        if (!isInsideCallback()) {
             throw new IllegalStateException(
                     "GL operations must not be performed outside a GL callback");
         }
     }
 
-    private void assertValidIds(int[] ids) {
+    // Throws if the provided array contains at least one invalid OpenGL id
+    private void assertValidIDs(int[] ids) {
         for (int id : ids) {
             if (id <= 0) {
                 throw new GLError("Unable to allocate GL object (returned id 0)");
@@ -73,23 +98,50 @@ public final class GLContext implements GLEventListener {
         }
     }
 
+    /**
+     * Returns whether the context has yet been initialized by a GLEventListener.init() callback.
+     * 
+     * @return Whether the context has been initialized.
+     */
     public boolean isInitialized() {
         return drawable != null;
     }
 
-    public boolean isInsideDisplayFunction() {
-        return drawable != null && Thread.currentThread().equals(glThread) && insideDisplay;
+    /**
+     * Returns whether the current thread is inside a GLEventListener callback and GL operations may
+     * be performed.
+     * 
+     * @return Whether the thread is inside a GL callback.
+     */
+    public boolean isInsideCallback() {
+        return drawable != null && Thread.currentThread().equals(glThread) && insideCallback;
     }
 
+    /**
+     * Returns the drawable's size.
+     * 
+     * @return The size, in pixels.
+     */
     public Dimension getSize() {
         assertIsInitialized();
         return new Dimension(drawable.getWidth(), drawable.getHeight());
     }
 
+    /**
+     * Loads a texture, provided as a bitmap, into GL memory, returning it's id.
+     * 
+     * @param image The image data. Must not be null and must follow the format provided.
+     * @param format The format of the provided image data.
+     * @param internalFormat The internal image format to be used by OpenGL.
+     * @param width The image's width, in pixels. Must be greater than zero.
+     * @param height The image's height, in pixels. Must be greater than zero.
+     * @param mipmaps Whether mipmaps should be generated and used when drawing.
+     * @return The OpenGL texture ID.
+     */
     public int loadTexture(byte[] image, int format, int internalFormat, int width, int height,
             boolean mipmaps) {
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         if (image == null || width <= 0 || height <= 0) {
             throw new IllegalArgumentException();
@@ -98,7 +150,7 @@ public final class GLContext implements GLEventListener {
         int[] ids = new int[1];
         gl.glGenTextures(1, ids, 0);
         GLError.throwIfActive(gl);
-        assertValidIds(ids);
+        assertValidIDs(ids);
 
         gl.glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height,
                 0, format, GL_UNSIGNED_BYTE, ByteBuffer.wrap(image));
@@ -122,9 +174,14 @@ public final class GLContext implements GLEventListener {
         return ids[0];
     }
 
+    /**
+     * Removes an existing OpenGL texture from graphics memory.
+     * 
+     * @param id The texture's id.
+     */
     public void deleteTexture(int id) {
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         if (id <= 0) {
             throw new IllegalArgumentException();
@@ -135,19 +192,25 @@ public final class GLContext implements GLEventListener {
         GLError.throwIfActive(gl);
     }
 
+    /**
+     * Loads a mesh into OpenGL memory, returning a vertex buffer object.
+     * 
+     * @param mesh The mesh to load. Must be a valid mesh.
+     * @return The vertex buffer object.
+     */
     public VertexBuffer loadMesh(Mesh mesh) {
         if (mesh == null || mesh.indices == null || mesh.indexCount < 0 || mesh.vertices == null) {
             throw new IllegalArgumentException();
         }
 
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         // Allocate vertex and index buffer
         int[] buffers = new int[2];
         gl.glGenBuffers(2, buffers, 0);
         GLError.throwIfActive(gl);
-        assertValidIds(buffers);
+        assertValidIDs(buffers);
 
         VertexBuffer vbo = new VertexBuffer(mesh.primitiveType, mesh.indexCount, buffers[0],
                 buffers[1]);
@@ -173,25 +236,36 @@ public final class GLContext implements GLEventListener {
         return vbo;
     }
 
+    /**
+     * Removes an existing vertex buffer object from graphics memory.
+     * 
+     * @param vbo The vertex buffer object. Must not be null and must hold valid buffer IDs.
+     */
     public void deleteVertexBuffer(VertexBuffer vbo) {
         if (vbo == null || vbo.indices <= 0 || vbo.vertices <= 0) {
             throw new IllegalArgumentException();
         }
 
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         gl.glDeleteBuffers(2, new int[] { vbo.vertices, vbo.indices }, 0);
         GLError.throwIfActive(gl);
     }
 
+    /**
+     * Loads a matrix into a given matrix slot (also called "matrix stack").
+     * 
+     * @param slot The slot, e.g. GL_MODELVIEW or GL_PROJECTION.
+     * @param matrix The matrix to load. Must not be null.
+     */
     public void loadMatrix(int slot, Matrix4 matrix) {
         if (matrix == null) {
             throw new IllegalArgumentException();
         }
 
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         gl.glMatrixMode(slot);
         GLError.throwIfActive(gl);
@@ -200,13 +274,19 @@ public final class GLContext implements GLEventListener {
         GLError.throwIfActive(gl);
     }
 
+    /**
+     * Draws a vertex buffer object with a given texture.
+     * 
+     * @param vbo The vertex buffer object. Must not be null and must hold valid buffer IDs.
+     * @param texture The texture ID to use. Must be greater than zero.
+     */
     public void drawVertexBuffer(VertexBuffer vbo, int texture) {
-        if (vbo == null || texture <= 0) {
+        if (vbo == null || vbo.indices <= 0 || vbo.vertices <= 0 || texture <= 0) {
             throw new IllegalArgumentException();
         }
 
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         gl.glBindTexture(GL_TEXTURE_2D, texture);
         GLError.throwIfActive(gl);
@@ -259,25 +339,76 @@ public final class GLContext implements GLEventListener {
         GLError.throwIfActive(gl);
     }
 
+    /**
+     * Clears the OpenGL color and depth buffer.
+     */
     public void clear() {
         assertIsInitialized();
-        assertIsInsideDisplayFunction();
+        assertIsInsideCallback();
 
         gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
+    /**
+     * Enables (glEnable) or disables (glDisable) a feature.
+     * 
+     * @param feature The OpenGL feature, e.g. GL_DEPTH_TEST
+     * @param enabled Whether to enable or disable the feature.
+     */
+    public void setFeatureEnabled(int feature, boolean enabled) {
+        assertIsInitialized();
+        assertIsInsideCallback();
+
+        if (enabled) {
+            gl.glEnable(feature);
+        } else {
+            gl.glDisable(feature);
+        }
+        GLError.throwIfActive(gl);
+    }
+
+    /**
+     * Sets the front- and backface polygon mode.
+     * 
+     * @param mode The mode, e.g. GL_FILL or GL_LINE
+     */
+    public void setPolygonMode(int mode) {
+        assertIsInitialized();
+        assertIsInsideCallback();
+
+        gl.glPolygonMode(GL_FRONT_AND_BACK, mode);
+        GLError.throwIfActive(gl);
+    }
+
+    /**
+     * Enqueues a runnable to be executed at the beginning of the next frame. On completion,
+     * <code>null</code> is passed to the listener.
+     * 
+     * @param runnable The runnable. Must not be null.
+     * @param listener The listener to call when execution has completed. May be null.
+     */
     public void invokeLater(Runnable runnable, RunnableResultListener listener) {
+        if (runnable == null) {
+            throw new IllegalArgumentException();
+        }
+
         synchronized (pendingInvocations) {
             pendingInvocations.add(new Invocation(runnable, listener));
         }
         postRedisplay();
     }
 
+    /**
+     * Enqueues a runnable to be executed at the beginning of the next frame.
+     * 
+     * @param runnable The runnable. Must not be null.
+     */
     public void invokeLater(Runnable runnable) {
         invokeLater(runnable, null);
     }
 
 
+    // For using a RunnableWithResult as a Runnable
     private static class RunnableResultAdapter implements Runnable {
 
         public Object result;
@@ -296,21 +427,40 @@ public final class GLContext implements GLEventListener {
     };
 
 
+    /**
+     * Enqueues a runnable to be executed at the beginning of the next frame.
+     * 
+     * @param runnable The runnable. Must not be null.
+     * @param listener The listener to call when execution has completed. May be null.
+     */
     public void invokeLater(final RunnableWithResult runnable, RunnableResultListener listener) {
         RunnableResultAdapter wrapper = new RunnableResultAdapter(runnable);
         invokeLater(wrapper, listener);
     }
 
+    /**
+     * Enqueues a runnable to be executed at the beginning of the next frame.
+     * 
+     * @param runnable The runnable. Must not be null.
+     */
     public void invokeLater(RunnableWithResult runnable) {
         invokeLater(runnable, null);
     }
 
+    /**
+     * Calls the runnable right away if inside a GL callback and enqueues it via invokeLater()
+     * otherwise.
+     * 
+     * @param runnable The runnable. must not be null.
+     * @param listener The listener to call when execution has completed. May be null, and might be
+     *        called before invokeSooner() returns.
+     */
     public void invokeSooner(Runnable runnable, RunnableResultListener listener) {
         if (runnable == null) {
             throw new IllegalArgumentException();
         }
 
-        if (isInsideDisplayFunction()) {
+        if (isInsideCallback()) {
             runnable.run();
             if (listener != null) {
                 listener.runnableCompleted(null);
@@ -320,8 +470,16 @@ public final class GLContext implements GLEventListener {
         }
     }
 
+    /**
+     * Calls the runnable right away if inside a GL callback and enqueues it via invokeLater()
+     * otherwise.
+     * 
+     * @param runnable The runnable. must not be null.
+     * @param listener The listener to call when execution has completed. May be null, and might be
+     *        called before invokeSooner() returns.
+     */
     public void invokeSooner(RunnableWithResult runnable, RunnableResultListener listener) {
-        if (isInsideDisplayFunction()) {
+        if (isInsideCallback()) {
             Object result = runnable.run();
             if (listener != null) {
                 listener.runnableCompleted(result);
@@ -331,9 +489,12 @@ public final class GLContext implements GLEventListener {
         }
     }
 
+    // Does things necessary at the beginning of every GL callback, and calls
+    // GLContextListener.beginFrame() if necessary
     private void beginDisplay() {
-        insideDisplay = true;
+        insideCallback = true;
 
+        // Invoke all pending invokeLater()s.
         ArrayList<Invocation> pendingCopy;
         synchronized (pendingInvocations) {
             pendingCopy = pendingInvocations;
@@ -351,6 +512,7 @@ public final class GLContext implements GLEventListener {
             }
         }
 
+        // Some callbacks, like initialize(), will not end the frame they begun
         if (!insideFrame) {
             insideFrame = true;
             for (GLContextListener l : listeners) {
@@ -359,6 +521,8 @@ public final class GLContext implements GLEventListener {
         }
     }
 
+    // Does things necessary at the end of every GL callback, and calls
+    // GLContextListener.endFrame().
     private void endDisplay(boolean endFrame) {
         if (endFrame && insideFrame) {
             insideFrame = false;
@@ -366,7 +530,7 @@ public final class GLContext implements GLEventListener {
                 l.endFrame(this);
             }
         }
-        insideDisplay = false;
+        insideCallback = false;
     }
 
     @Override
@@ -425,38 +589,54 @@ public final class GLContext implements GLEventListener {
         endDisplay(false);
     }
 
+    /**
+     * Adds a @link{GLContextListener}.
+     * 
+     * @param l The listener.
+     */
     public synchronized void addGLContextListener(GLContextListener l) {
         listeners.add(l);
     }
 
+    /**
+     * Removes a @link{GLContextListener}.
+     * 
+     * @param l The listener.
+     */
     public synchronized void removeGLContextListener(GLContextListener l) {
         while (listeners.remove(l));
     }
 
     /**
-     * Notifies the {@link de.joglearth.rendering.Renderer} that a new frame should be rendered. If
-     * <code>start()</code> is called this method may have no effect. Asynchronous method, does not
-     * wait until a frame is drawn.
+     * Notifies the context that at least one more frame should be drawn. The drawing is done
+     * asynchronously and the function returns immediately.
      */
     public void postRedisplay() {
+        // When the initialization occurs, a frame will be drawn anyway.
         if (isInitialized()) {
             synchronized (this) {
-                isPosted = true;
-                if (isRunning) {
+                redisplayPending = true;
+                if (renderingLoopActive) {
                     return;
                 }
-                isRunning = true;
+                renderingLoopActive = true;
             }
 
-            AWTInvoker.invoke(new Runnable() {
+            // Call invokeLater() while there are pending frames. Don't use a loop so that other
+            // AWT events can be processed as well.
+            AWTInvoker.invokeLater(new Runnable() {
 
                 @Override
                 public void run() {
-                    do {
-                        isPosted = false;
-                        drawable.display();
-                    } while (isPosted);
-                    isRunning = false;
+                    redisplayPending = false;
+                    drawable.display();
+                    
+                    // might be re-set in another thread
+                    if (redisplayPending) {
+                        AWTInvoker.invokeLater(this);
+                    } else {
+                        renderingLoopActive = false;
+                    }
                 }
             });
         }
--- Spezifikation/src/de/joglearth/util/AWTInvoker.java
+++ Spezifikation/src/de/joglearth/util/AWTInvoker.java
@@ -23,22 +23,64 @@ public final class AWTInvoker {
             }
         }
     }
+    
+    private static class RunnableResultWrapper implements Runnable {
 
-    public static Object invoke(final RunnableWithResult runnable) throws Throwable {
-        class RunnableWrapper implements Runnable {
-
-            public Object result;
-
-
-            @Override
-            public void run() {
-                result = runnable.run();
-            }
-
+        public RunnableWithResult runnable;
+        public Object result;
+        
+        public RunnableResultWrapper(RunnableWithResult r) {
+            runnable = r;
         }
-        ;
-        RunnableWrapper wrapper = new RunnableWrapper();
+
+        @Override
+        public void run() {
+            result = runnable.run();
+        }
+    }
+    
+    private static class RunnableCallbackWrapper implements Runnable {
+        
+        private Object runnable;
+        private RunnableResultListener callback;
+        
+        public RunnableCallbackWrapper(Object r, RunnableResultListener l) {
+            runnable = r;
+            callback = l;
+        }
+        
+        @Override
+        public void run() {
+            if (runnable instanceof Runnable) {
+                ((Runnable) runnable).run();
+                callback.runnableCompleted(null);
+            } else if (runnable instanceof RunnableWithResult) {
+                callback.runnableCompleted(((RunnableWithResult) runnable).run());
+            }
+        }
+    }
+    
+
+    public static Object invoke(RunnableWithResult runnable) throws Throwable {
+        RunnableResultWrapper wrapper = new RunnableResultWrapper(runnable);
         invoke(wrapper);
         return wrapper.result;
     }
+
+    public static void invokeLater(Runnable runnable) {
+        EventQueue.invokeLater(runnable);
+    }
+    
+    public static void invokeLater(Runnable runnable, RunnableResultListener l) {
+        EventQueue.invokeLater(runnable);
+    }
+    
+    public static void invokeLater(RunnableWithResult runnable) {
+        EventQueue.invokeLater(new RunnableCallbackWrapper(runnable, null));
+    }
+    
+    public static void invokeLater(RunnableWithResult runnable, RunnableResultListener l) {
+        EventQueue.invokeLater(new RunnableCallbackWrapper(runnable, l));
+    }
+    
 }
