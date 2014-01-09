27cf11466b898787d196b1edbc4ccf8a4f2339dd
Constantin Wenger <constantin.wenger@googlemail.com>
2014-01-10 00:20:13 +0100

    added JUnitTestClasses

---
+++ Spezifikation/src/de/joglearth/location/nominatim/NominatimSourceTest.java
@@ -0,0 +1,30 @@
+package de.joglearth.location.nominatim;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class NominatimSourceTest {
+
+    @Test
+    public final void testNominatimSource() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testRequestObject() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testReverseSearch() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testDispose() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
