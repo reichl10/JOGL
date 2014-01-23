5bc4b3e1c63b270c72a871e5df9dcc68e8e6f57e
Constantin Wenger <constantin.wenger@googlemail.com>
2014-01-23 20:14:47 +0100

    VertexBufferCacheTest fixes

--- Spezifikation/src/de/joglearth/junit/source/opengl/VetexBufferCacheTest.java
+++ Spezifikation/src/de/joglearth/junit/source/opengl/VetexBufferCacheTest.java
@@ -1,6 +1,8 @@
 package de.joglearth.junit.source.opengl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -16,8 +18,8 @@ public class VetexBufferCacheTest {
     GLTestWindow window = new GLTestWindow();
     @Test
     public void testDropObject() {
-        VertexBufferPool<Integer> vbc = new VertexBufferPool<Integer>(window.getGL());
-        vbc.putObject(new Integer(10), new VertexBuffer());
+        VertexBufferPool<Integer> vbc = new VertexBufferPool<Integer>(window.getGLContext());
+        vbc.putObject(new Integer(10), new VertexBuffer(0, 0, 0, 0));
         SourceResponse<VertexBuffer> res = vbc.requestObject(new Integer(10), null);
         assertTrue(res.response == SourceResponseType.SYNCHRONOUS);
         assertNotNull(res.value);
