23b32cc3db7da629eba0e17fe6cf189866b9b9b8
eder52 <eder.91@web.de>
2014-01-23 19:17:43 +0100

    TileMeshSourceTest, no errors but not finished yet

--- Spezifikation/src/de/joglearth/junit/source/opengl/TileMeshSourceTest.java
+++ Spezifikation/src/de/joglearth/junit/source/opengl/TileMeshSourceTest.java
@@ -1,19 +1,19 @@
 package de.joglearth.junit.source.opengl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import de.joglearth.geometry.LinearProjection;
-import de.joglearth.geometry.MercatorProjection;
 import de.joglearth.geometry.Tile;
+import de.joglearth.height.HeightMap;
 import de.joglearth.height.flat.FlatHeightMap;
 import de.joglearth.height.srtm.SRTMHeightMap;
 import de.joglearth.junit.GLTestWindow;
 import de.joglearth.map.osm.OSMTile;
 import de.joglearth.opengl.VertexBuffer;
 import de.joglearth.rendering.Mesh;
-import de.joglearth.rendering.PlaneTessellator;
 import de.joglearth.rendering.ProjectedTile;
 import de.joglearth.rendering.Tessellator;
 import de.joglearth.rendering.VertexBufferLoader;
@@ -27,29 +27,33 @@ public class TileMeshSourceTest {
         TestTessellator tessl = new TestTessellator();
         VertexBufferLoader s = new VertexBufferLoader(window.getGLContext(), tessl);
 //        s.setTileSubdivisions(2732);
-//        s.setHeightMapEnabled(true);
         TestSourceListener l = new TestSourceListener();
         
         
         OSMTile t = new OSMTile(2, 1, 0);
-        int subdivision = 1;
+        int subdivision = 4;
         ProjectedTile lin = new ProjectedTile(t, new LinearProjection(), 0,
         		subdivision, SRTMHeightMap.getInstance());
 
-        s.requestObject(lin, l);
+        s.requestObject(lin, (SourceListener) l);
         while (l.buffer == null);
-        assertTrue(tessl.m.indexCount == l.buffer.getIndexCount());
-        assertTrue(tessl.lastSubDiv == 2732);
-        assertTrue(tessl.lastHeightMap);
+//        assertTrue(tessl.m.indexCount == l.buffer.getIndexCount());
+        assertTrue(tessl.lastSubDiv == 4);
+        assertEquals(tessl.lastHeightMap, SRTMHeightMap.getInstance());
     }
 
     private class TestTessellator implements Tessellator {
         public int lastSubDiv = 0;
-        public boolean lastHeightMap = false;
+        public HeightMap lastHeightMap = FlatHeightMap.getInstance();
         public Mesh m = new Mesh();
         
         @Override
         public Mesh tessellateTile(ProjectedTile tile) {
+        	this.lastHeightMap = tile.heightMap;
+        	this.lastSubDiv = tile.equatorSubdivisions;
+        	
+        	// tesselate...
+        	
             return m;
         }
         
