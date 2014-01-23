5c2c75277059afb9ec31158289aa1b1e9175c3b5
Gabi <haasgab@fim.uni-passau.de>
2014-01-23 15:00:15 +0100

    OSMPathTranslatorTest JUnit Test Fehler korrigiert

--- Spezifikation/src/de/joglearth/junit/source/osm/OSMPathTranslatorTest.java
+++ Spezifikation/src/de/joglearth/junit/source/osm/OSMPathTranslatorTest.java
@@ -1,30 +1,28 @@
 package de.joglearth.junit.source.osm;
 
-import static org.junit.Assert.*;
-
-import java.io.File;
-import java.util.regex.Pattern;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import de.joglearth.source.osm.OSMTile;
-import de.joglearth.source.tiles.osm.OSMPathTranslator;
-import de.joglearth.surface.TiledMapType;
-import de.joglearth.tiles.Tile;
+import de.joglearth.map.MapConfiguration;
+import de.joglearth.map.TileName;
+import de.joglearth.map.osm.OSMMapConfiguration;
+import de.joglearth.map.osm.OSMMapType;
+import de.joglearth.map.osm.OSMPathTranslator;
+import de.joglearth.map.osm.OSMTile;
 
 
 public class OSMPathTranslatorTest {
 
     @Test
     public void test() {
-        //TODO System.out.println("[0-9a-fA-F]{3}"
-            + Pattern.quote(File.separator) + "([A-Z_]+)-([0-9]+)-([0-9]+)-([0-9]+)\\.png");
-        OSMPathTranslator trans = new OSMPathTranslator();
-        OSMTile testTile = new OSMTile(new Tile(3, 1, 1), TiledMapType.OSM2WORLD);
-        String s = trans.toFileSystemPath(testTile);
-        OSMTile tile = trans.fromFileSystemPath(s);
-        assertEquals(testTile, tile);
-        
-    }
 
+        OSMPathTranslator trans = new OSMPathTranslator();
+        MapConfiguration config = new OSMMapConfiguration(OSMMapType.OSM2WORLD);
+        OSMTile testTile = new OSMTile(3, 1, 1);
+        TileName testName = new TileName(config, testTile);
+        
+        String s = trans.toFileSystemPath(testName);
+        TileName resultTileName = trans.fromFileSystemPath(s);
+        assertEquals(testName, resultTileName);
+    }
 }
