b477bd83fafb6e3a05d381795c74324476f61d2b
Fabian Knorr <ers.trion@gmail.com>
2014-01-25 19:48:34 +0100

    Priorized / PriorizedQueue

---
+++ Spezifikation/src/de/joglearth/source/Priorized.java
@@ -0,0 +1,12 @@
+package de.joglearth.source;
+
+
+public interface Priorized {
+
+    public void setPriority(long priority);
+    
+    public long getPriority();    
+    
+    public void increasePriority();
+        
+}
